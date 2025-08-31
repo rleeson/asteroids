3d95f94f48af5b56d21dcead9e0cd690cbe19d85
Ryan Leeson <sysadmin@ryanleeson.com>
2025-08-31 13:37:06 -0400

    Move event handling to subroutine

--- main.py
+++ main.py
@@ -4,19 +4,24 @@ import pygame
 from constants import * 
 
 def game_loop(screen):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return 0
+    
     screen.fill("black")
     pygame.display.flip()
+
+    return 1
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return;
+        game_state = game_loop(screen)
 
-        game_loop(screen)
+        if game_state == 0:
+            return
 
 if __name__ == "__main__":
     main()
