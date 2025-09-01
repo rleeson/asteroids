4b2a83c45c5cf5d35f229da7264b42bc887fb16c
Ryan Leeson <sysadmin@ryanleeson.com>
2025-09-01 14:27:44 -0400

    Add base player model and movement to the game

--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 # throughout this file
 import pygame
 from constants import * 
+from player import Player
 
-def game_loop(screen):
+def game_loop(screen, dt, player):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return 0
     
     screen.fill("black")
+    player.update(dt)
+    player.draw(screen)
+
     pygame.display.flip()
 
     return 1
@@ -16,9 +20,13 @@ def game_loop(screen):
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
-        game_state = game_loop(screen)
+        game_state = game_loop(screen, dt, player)
+        dt = clock.tick(60) / 1000
 
         if game_state == 0:
             return
